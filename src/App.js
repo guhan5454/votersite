1d80ef5da952a0ac85cf5634fdaefc1bceb54109
guhan5454 <guhan5454@gmail.com>
2023-07-16 17:26:51 +0530

    basic UI changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import logo from './Assests/blank-profile-picture-973460_1280.webp'
 
 const App = () => {
   const [currentStep, setCurrentStep] = useState(1);
@@ -34,7 +35,43 @@ const App = () => {
           <fieldset>
             <h2 className="fs-title">Chairman</h2>
             <h3 className="fs-subtitle">Select any one</h3>
-            <imput type="radio" name="guhan" value="guhan" className="radio-button" checked={selectedPerson.chairman === "guhan"} onChange={handleChange} />
+            <div className='buttonContainer'>
+              <div className='inputfield'>
+                <img alt=''  className='profile' src={logo} ></img>
+                <label>
+                  Guhan
+                </label>
+                <input type="radio" name="guhan" value="guhan" className="radioButton" checked={selectedPerson.chairman === "guhan"} onChange={handleChange} />
+              </div>
+              <div className='inputfield'>
+                <img alt=''  className='profile' src={logo} ></img>
+                <label>
+                  Guhan
+                </label>
+                <input type="radio" name="guhan" value="guhan" className="radioButton" checked={selectedPerson.chairman === "guhan"} onChange={handleChange} />
+              </div>
+              <div className='inputfield'>
+                <img alt=''  className='profile' src={logo} ></img>
+                <label>
+                  Guhan
+                </label>
+                <input type="radio" name="guhan" value="guhan" className="radioButton" checked={selectedPerson.chairman === "guhan"} onChange={handleChange} />
+              </div>
+              <div className='inputfield'>
+                <img alt=''  className='profile' src={logo} ></img>
+                <label>
+                  Guhan
+                </label>
+                <input type="radio" name="guhan" value="guhan" className="radioButton" checked={selectedPerson.chairman === "guhan"} onChange={handleChange} />
+              </div>
+              <div className='inputfield'>
+                <img alt=''  className='profile' src={logo} ></img>
+                <label>
+                  Guhan
+                </label>
+                <input type="radio" name="guhan" value="guhan" className="radioButton" checked={selectedPerson.chairman === "guhan"} onChange={handleChange} />
+              </div>
+            </div>
             {/* <input type="text" name="email" placeholder="Email" className="input-field" />
             <input type="password" name="pass" placeholder="Password" className="input-field" />
             <input type="password" name="cpass" placeholder="Confirm Password" className="input-field" /> */}
@@ -56,16 +93,40 @@ const App = () => {
       case 3:
         return (
           <fieldset>
-            <h2 className="fs-title">Personal Details</h2>
-            <h3 className="fs-subtitle">We will never sell it</h3>
-            <input type="text" name="fname" placeholder="First Name" className="input-field" />
-            <input type="text" name="lname" placeholder="Last Name" className="input-field" />
-            <input type="text" name="phone" placeholder="Phone" className="input-field" />
-            <textarea name="address" placeholder="Address" className="input-field"></textarea>
+            <h2 className="fs-title">Social Profiles</h2>
+            <h3 className="fs-subtitle">Your presence on the social network</h3>
+            <input type="text" name="twitter" placeholder="Twitter" className="input-field" />
+            <input type="text" name="facebook" placeholder="Facebook" className="input-field" />
+            <input type="text" name="gplus" placeholder="Google Plus" className="input-field" />
             <input type="button" name="previous" className="previous action-button" value="Previous" onClick={handlePrevious} />
+            <input type="button" name="next" className="next action-button" value="Next" onClick={handleNext} />
+          </fieldset>
+        );  
+      case 4:
+        return (
+          <fieldset>
+          <h2 className="fs-title">Social Profiles</h2>
+          <h3 className="fs-subtitle">Your presence on the social network</h3>
+          <input type="text" name="twitter" placeholder="Twitter" className="input-field" />
+          <input type="text" name="facebook" placeholder="Facebook" className="input-field" />
+          <input type="text" name="gplus" placeholder="Google Plus" className="input-field" />
+          <input type="button" name="previous" className="previous action-button" value="Previous" onClick={handlePrevious} />
+          <input type="button" name="next" className="next action-button" value="Next" onClick={handleNext} />
+        </fieldset>
+        );
+      case 5:
+        return(
+          <fieldset>
+            <h2 className="fs-title">Social Profiles</h2>
+            <h3 className="fs-subtitle">Your presence on the social network</h3>
+            <input type="text" name="twitter" placeholder="Twitter" className="input-field" />
+            <input type="text" name="facebook" placeholder="Facebook" className="input-field" />
+            <input type="text" name="gplus" placeholder="Google Plus" className="input-field" />
+            <input type="button" name="previous" className="previous action-button" value="Previous" onClick={handlePrevious} />
+            <input type="button" name="next" className="next action-button" value="Next" onClick={handleNext} />
             <a href="https://twitter.com/GoktepeAtakan" className="submit action-button" target="_top">Submit</a>
           </fieldset>
-        );
+        )
       default:
         return null;
     }
@@ -77,7 +138,9 @@ const App = () => {
         <li className={currentStep === 1 ? 'active' : ''}>Account Setup</li>
         <li className={currentStep === 2 ? 'active' : ''}>Social Profiles</li>
         <li className={currentStep === 3 ? 'active' : ''}>Personal Details</li>
-      </ul>
+        <li className={currentStep === 4 ? 'active' : ''}>Personal Details</li>
+        <li className={currentStep === 5 ? 'active' : ''}>Personal Details</li>
+        <li className={currentStep === 6 ? 'active' : ''}>Personal Details</li>      </ul>
       {renderFieldset(currentStep)}
     </form>
   );
